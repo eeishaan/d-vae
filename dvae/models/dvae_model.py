2e68123830ada903195fdf394a7cd6525735c058
Ishaan Kumar <ishaan@MacBook-Pro-de-Ishaan.local>
2019-11-25 00:21:30 -0500

    correct encoder + decoder forward

--- dvae/models/dvae_model.py
+++ dvae/models/dvae_model.py
@@ -26,31 +26,31 @@ class Encoder(nn.Module):
         self.lin12 = nn.Linear(hidden_state_size, hidden_state_size)
 
     def forward(self, X):
-        dep_graph, node_encoding, node_order = X['graph'], X['node_encoding'], X['node_order']
+        dep_graph, node_encoding = X['graph'], X['node_encoding']
         batch_size, seq_len, _ = dep_graph.shape
         device = dep_graph.device
         node_hidden_state = torch.zeros(
             batch_size, seq_len, self.hidden_state_size, device=device)
 
-        for node_seq_index in node_order:
+        for index in range(seq_len):
             # TODO: add modification for NAS and bayesian task here
             # compute h_in
-            ancestors = dep_graph[:, node_seq_index]
+            ancestors = dep_graph[:, index]
             ancestor_hidden_state = node_hidden_state * \
                 ancestors.view(batch_size, -1, 1)
             ancestor_hidden_state = ancestor_hidden_state.view(
                 -1, self.hidden_state_size)
-            h_in = torch.sum(
-                self.gating_network(ancestor_hidden_state) *
-                self.mapping_network(ancestor_hidden_state),
-                dim=0)
+            h_in = self.gating_network(ancestor_hidden_state) * \
+                self.mapping_network(ancestor_hidden_state)
+            h_in = h_in.view(batch_size, -1, self.hidden_state_size)
+            h_in = torch.sum(h_in, dim=1)
 
             # comute hv
-            hv = self.gru(node_encoding[:, node_seq_index], h_in)
+            hv = self.gru(node_encoding[:, index], h_in)
             assert hv.shape == (batch_size, self.hidden_state_size), \
                 'Shape of hv is wrong, desired {} got {}'.format(
                     (batch_size, self.hidden_state_size), hv.shape)
-            node_hidden_state[:, node_seq_index] = hv
+            node_hidden_state[:, index] = hv
 
         mu = self.lin11(hv)
         logvar = self.lin12(hv)
@@ -61,20 +61,81 @@ class Decoder(nn.Module):
     def __init__(self, num_classes=7, hidden_state_size=501):
         super(Decoder, self).__init__()
         self.hidden_state_size = hidden_state_size
-
+        self.num_classes = num_classes
         self.lin1 = nn.Linear(hidden_state_size, hidden_state_size)
         self.add_vertex = nn.Sequential(
-            nn.Linear(hidden_state_size, hidden_state_size),
+            nn.Linear(hidden_state_size, num_classes),
             nn.Softmax()
         )
         self.add_edge = nn.Sequential(
             nn.Linear(2*hidden_state_size, 1),
             nn.Sigmoid()
         )
+        self.gating_network = nn.Sequential(
+            nn.Linear(hidden_state_size, hidden_state_size),
+            nn.Sigmoid()
+        )
+        self.mapping_network = nn.Linear(hidden_state_size, hidden_state_size)
         self.gru = nn.GRUCell(num_classes, hidden_state_size)
 
     def forward(self, z, X):
-        pass
+        dep_graph, node_encoding = X['graph'], X['node_encoding']
+
+        batch_size, seq_len, _ = dep_graph.shape
+        gen_dep_graph = torch.zeros_like(dep_graph)
+        gen_node_encoding = torch.zeros_like(node_encoding)
+
+        device = dep_graph.device
+        node_hidden_state = torch.zeros(
+            batch_size, seq_len, self.hidden_state_size, device=device)
+
+        graph_state = self.lin1(z)
+
+        for index in range(seq_len):
+
+            # sample node type
+            new_node_type_logits = self.add_vertex(graph_state)
+            gen_node_encoding[:, index, :] = new_node_type_logits
+
+            # compute initial hidden states
+            if index == 0:
+                h_in = graph_state
+            else:
+                pass
+                # TODO: Compute initial h_v
+
+            # sample edges
+            for v_j in range(index-1, -1, -1):
+                hidden_j = node_hidden_state[:, v_j]
+                is_edge = self.add_edge(torch.cat([h_v, hidden_j], dim=0))
+                is_edge[is_edge < 0.5] = 0
+                is_edge[is_edge >= 0.5] = 1
+                gen_dep_graph[:, index, v_j] = is_edge
+
+                # TODO: add modification for NAS and bayesian task here
+                # compute h_in
+                ancestors = dep_graph[:, index]
+                # warning verify if happening in-place
+                # zero out all ancestors before v_j
+                ancestors[:, :v_j] = 0
+                ancestor_hidden_state = node_hidden_state * \
+                    ancestors.view(batch_size, -1, 1)
+                ancestor_hidden_state = ancestor_hidden_state.view(
+                    -1, self.hidden_state_size)
+                h_in = self.gating_network(ancestor_hidden_state) * \
+                    self.mapping_network(ancestor_hidden_state)
+                h_in = h_in.view(batch_size, -1, self.hidden_state_size)
+                h_in = torch.sum(h_in, dim=1)
+
+                # comute hv
+                hv = self.gru(node_encoding[:, index], h_in)
+                assert hv.shape == (batch_size, self.hidden_state_size), \
+                    'Shape of hv is wrong, desired {} got {}'.format(
+                        (batch_size, self.hidden_state_size), hv.shape)
+                node_hidden_state[:, index] = hv
+            node_hidden_state[:, index] = hv
+            graph_state = hv
+        return gen_dep_graph, gen_node_encoding
 
 
 class Dvae(pl.LightningModule):
