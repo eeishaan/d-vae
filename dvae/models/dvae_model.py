4f408da1c3ecab4d55cadebfaed8cc69592e2a4d
Ishaan Kumar <ishaan@MacBook-Pro-de-Ishaan.local>
2019-11-29 12:16:32 -0500

    optimizer changes

--- dvae/models/dvae_model.py
+++ dvae/models/dvae_model.py
@@ -194,9 +194,9 @@ class Dvae(pl.LightningModule):
 
     def configure_optimizers(self):
         # REQUIRED
-        # can return multiple optimizers and learning_rate schedulers
-        # (LBFGS it is automatically supported, no need for closure function)
-        return torch.optim.Adam(self.parameters(), lr=0.02)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+        lr_schedule = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
+        return [optimizer], [lr_schedule]
 
     @pl.data_loader
     def train_dataloader(self):
