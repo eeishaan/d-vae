9a67a40647d0ac34fd6936e07a3221c36d71e4d0
Ishaan Kumar <ishaan@MacBook-Pro-de-Ishaan.local>
2019-11-29 12:34:34 -0500

    train script

---
+++ train.py
@@ -0,0 +1,18 @@
+import os
+
+from pytorch_lightning import Trainer
+
+from dvae.constants import BATCH_SIZE, DATA_DIR, MODEL_DIR
+from dvae.models.dvae_model import Dvae
+
+model = Dvae(batch_size=BATCH_SIZE, dataset_file=os.path.join(
+    DATA_DIR, 'final_structures6.txt'))
+
+trainer = Trainer(
+    default_save_path=MODEL_DIR,
+    show_progress_bar=True,
+    early_stop_callback=None,
+    max_nb_epochs=100,
+    gpus=1,
+    distributed_backend='ddp')
+trainer.fit(model)
