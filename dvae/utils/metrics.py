5727ad91fcb645f6a1b3b6f889b282d98e5ede8d
Ishaan Kumar <ishaan@MacBook-Pro-de-Ishaan.local>
2019-12-08 12:16:38 -0500

    correct accuracy computation

--- dvae/utils/metrics.py
+++ dvae/utils/metrics.py
@@ -4,9 +4,9 @@ import torch
 def reconstruction_accuracy(true_encodings, true_deps, pred_encodings, pred_deps):
     """
     true_encodings: Tensors containing  true node encoding of graph.
-                    Shape: (batch_size, node_types)
+                    Shape: (batch_size, seq_len, node_types)
     pred_encodings: Tensors containing  predicted node encoding of graph
-                    (batch_size, sample_num, node_types). sample_num = 
+                    (batch_size, sample_num, seq_len, node_types). sample_num = 
                     decoded graphs for the same input graph.
     true_deps: Tensor containing true dependency graph.
                 Shape: (batch_size, seq_len, seq_len)
@@ -14,12 +14,13 @@ def reconstruction_accuracy(true_encodings, true_deps, pred_encodings, pred_deps
                 Shape: (batch_size, sample_num, seq_len, seq_len)
     """
     # first check if node order is correct
-    batch_size, sample_num, _ = pred_encodings.shape
+    batch_size, sample_num, _, _ = pred_encodings.shape
     node_equality = (true_encodings.unsqueeze(1) == pred_encodings)
-    node_equality = node_equality.all(dim=-1)
+    # first check if node type is same then check if all nodes are in the
+    # seq are same
+    node_equality = node_equality.all(dim=-1).all(dim=-1).unsqueeze(-1)
 
     # check if correct nodes have correct edges
-    node_equality.unsqueeze_(-1)
     true_deps = true_deps.view(batch_size, -1).unsqueeze(1)
     pred_deps = pred_deps.view(batch_size, sample_num, -1)
     edge_equality = (true_deps == pred_deps) * node_equality
