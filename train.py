3f9e58c0d977d4bc748bcd0b7cb91028dfd035fd
Ishaan Kumar <ishaan@MacBook-Pro-de-Ishaan.local>
2019-12-14 16:03:13 -0500

    train script update

--- train.py
+++ train.py
@@ -6,17 +6,30 @@ from pytorch_lightning import Trainer
 
 from dvae.constants import BATCH_SIZE, DATA_DIR, MODEL_DIR
 from dvae.models.dvae_model import Dvae
+from dvae.models.svae_model import Svae
 
+# hparams = {
+#     'batch_size': BATCH_SIZE,
+#     'dataset_file': os.path.join(DATA_DIR, 'asia_200k.txt'),
+#     'mod': 'bn',
+#     'beta': 0.005,
+#     'task_type': 'bn',
+#     'num_classes': 8+2,
+#     'max_seq': 8+2,
+#     'bidirectional': True,
+#     'same_mapper_gater': True
+# }
 hparams = {
     'batch_size': BATCH_SIZE,
-    'dataset_file': os.path.join(DATA_DIR, 'final_structures6.txt'),
-    'mod': None,
+    'dataset_file': os.path.join(DATA_DIR, 'asia_200k.txt'),
     'beta': 0.005,
-    'bidirectional': True
+    'num_classes': 8+2,
+    'task_type': 'bn',
+    'max_seq': 8+2,
 }
 hparams = Namespace(**hparams)
 
-model = Dvae(hparams)
+model = Svae(hparams)
 
 trainer = Trainer(
     default_save_path=MODEL_DIR,
