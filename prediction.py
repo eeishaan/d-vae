18d02b96fa5437ff68305666aad916a3c8d128df
Swechha <swechha16.05@gmail.com>
2019-12-13 19:57:17 -0500

    add prediction script

--- prediction.py
+++ prediction.py
@@ -12,51 +12,81 @@ from dvae.models.svae_model import Svae
 from dvae.utils.dataloader import get_dataloaders
 
 checkpoint = torch.load(
-    "dvae/checkpoints/lightning_logs/version_0/checkpoints/_ckpt_epoch_107.ckpt",
-    map_location=torch.device("cpu"),
+    "dvae/checkpoints/high_lr/_ckpt_epoch_157.ckpt", map_location=torch.device("cpu")
 )
 
 a = Namespace(**{"dataset_file": None, "mod": None, "bidirectional": True, "sgp": True})
-model = Svae(a)
-# model = Dvae(a)
+# model = Svae(a)
+model = Dvae(a)
 
 model.load_state_dict(checkpoint["state_dict"])
 model.eval()
 model.freeze()
 
 dataset_file = os.path.join(DATA_DIR, "final_structures6.txt")
-train_loader, val_loader, test_loader = get_dataloaders(
-    BATCH_SIZE, dataset_file, fmt="str"
-)
-# train_loader, val_loader, test_loader = get_dataloaders(BATCH_SIZE, dataset_file)
+# train_loader, val_loader, test_loader = get_dataloaders(
+#     BATCH_SIZE, dataset_file, fmt="str"
+# )
+train_loader, val_loader, test_loader = get_dataloaders(BATCH_SIZE, dataset_file)
 acc = 0
-# y_train = []
-# latent_vectors = []
-# for batch in val_loader[0]:
-#     true_acc = batch["acc"]
-#     _, mu, logvar = model.encoder(batch)
-#     z = model.reparamterize(mu, logvar)
-#     y_train.append(true_acc.detach().numpy())
-#     latent_vectors.append(z.detach().numpy())
+y_train = []
+latent_vectors = []
+for batch in val_loader[0]:
+    true_acc = batch["acc"]
+    _, mu, logvar = model.encoder(batch)
+    z = model.reparamterize(mu, logvar)
+    y_train.append(true_acc.detach().numpy())
+    latent_vectors.append(z.detach().numpy())
 
-# print(latent_vectors[0].shape)
-# print(len(latent_vectors))
-# pickle.dump(latent_vectors, open(f"./val_latent_rep.pkl", "wb"))
-# pickle.dump(y_train, open(f"./val_accs.pkl", "wb"))
+print(latent_vectors[0].shape)
+print(len(latent_vectors))
+pickle.dump(latent_vectors, open(f"./val_latent_rep.pkl", "wb"))
+pickle.dump(y_train, open(f"./val_accs.pkl", "wb"))
 
-for x in test_loader:
-    res = model.model_eval(x, 10, 10)
-    acc += res["recon_acc"]
-acc / len(test_loader)
+acc = 0
+y_train = []
+latent_vectors = []
+for batch in train_loader:
+    true_acc = batch["acc"]
+    _, mu, logvar = model.encoder(batch)
+    z = model.reparamterize(mu, logvar)
+    y_train.append(true_acc.detach().numpy())
+    latent_vectors.append(z.detach().numpy())
 
-g = x["graph"]
-n = x["node_encoding"]
+print(latent_vectors[0].shape)
+print(len(latent_vectors))
+pickle.dump(latent_vectors, open(f"./train_latent_rep.pkl", "wb"))
+pickle.dump(y_train, open(f"./train_accs.pkl", "wb"))
 
-g_0 = torch.cat([g[0].unsqueeze(0)] * 10, dim=0)
-n_0 = torch.cat([n[0].unsqueeze(0)] * 10, dim=0)
+acc = 0
+y_train = []
+latent_vectors = []
+for batch in test_loader:
+    true_acc = batch["acc"]
+    _, mu, logvar = model.encoder(batch)
+    z = model.reparamterize(mu, logvar)
+    y_train.append(true_acc.detach().numpy())
+    latent_vectors.append(z.detach().numpy())
 
-inp_0 = {"graph": g_0, "node_encoding": n_0}
-out_0 = model.predict(inp_0)
-# out_0
-# g_0
-# n_0
+print(latent_vectors[0].shape)
+print(len(latent_vectors))
+pickle.dump(latent_vectors, open(f"./test_latent_rep.pkl", "wb"))
+pickle.dump(y_train, open(f"./test_accs.pkl", "wb"))
+
+# for x in test_loader:
+#     res = model.model_eval(x, 10, 10)
+#     acc += res["recon_acc"]
+# acc / len(test_loader)
+
+# g = x["graph"]
+# # n = x["node_encoding"]
+
+# g_0 = torch.cat([g[0].unsqueeze(0)] * 10, dim=0)
+# # n_0 = torch.cat([n[0].unsqueeze(0)] * 10, dim=0)
+
+# # inp_0 = {"graph": g_0, "node_encoding": n_0}
+# inp_0 = {"graph": g_0}
+# out_0 = model.predict(inp_0)
+# # out_0
+# # g_0
+# # n_0
